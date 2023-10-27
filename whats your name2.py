0e69b7b678592d6e8fb95bd433ecff93cf7cf106
SkylesssCoding <149161768+SkylesssCoding@users.noreply.github.com>
2023-10-27 14:01:15 +0300

    Add files via upload
    This is the code!

---
+++ whats your name2.py
@@ -0,0 +1,7 @@
+name = input(" What's your name?")
+
+print(f"Hello {name} nice to meet you!")
+
+name = input(f"Your name is really funny and i really like it. Do you have any other names?")
+
+print(f"{name} is your name? Damn that is a really nice name!")
